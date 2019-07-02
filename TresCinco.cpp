2d5f9e5001b1c127d5fb2920dc4c443971b6d343
Gerardo Medrano <gmedrano@auronix.com>
2019-07-02 14:00:51 -0500

    1.0

---
+++ TresCinco.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void Numeros(int final){
+	for(int i = 1; i<=final;i++){
+		if(i%3 ==0 )
+			printf("Tres");
+
+		if(i%5==0)
+			printf("Cinco");
+		if(i%3 != 0 && i%5 != 0){
+			for(int j = 0; j < i ; j++)
+				printf("%d ",i);
+		}
+		printf("Hola!!!\n");
+	}
+}
+
+
+int main(){
+	//holagit commit 
+	Numeros(650);
+	return 0;
+}
